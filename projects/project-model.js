4bf6edffbae83fa547888d79be084e3fb69a53b6
chrisdalao <chrisdalao@gmail.com>
2019-07-19 10:10:02 -0700

    completed GETALL, GETBYID, POST for Projects - GetByID is still a WIP

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,9 +1,33 @@
 const db = require('../data/db-config.js');
 
 module.exports = {
-    getProjects,
+    find,
+    findById,
+    insert,
+
 }
 
-function getProjects() {
+function find() {
     return db('projects');
+}
+
+function findById(id) {
+    return db('projects')
+        .where({ id })
+        .first()
+        .then(project => {
+            if (project) {
+                return project;
+            } else {
+                return null;
+            }
+        })
+}
+
+function insert(project) {
+    return db('projects')
+        .insert(project)
+        .then(id => {
+            return findById(id[0]);
+        });
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -3,7 +3,7 @@ const Projects = require('./project-model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Projects.getProjects()
+    Projects.find()
         .then(projects => {
             res.status(200).json(projects);
         })
@@ -11,5 +11,32 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: 'Failed to get projects' });
         })
 })
+
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Projects.findById(id)
+        .then(project => {
+            if (project) {
+                res.status(200).json(project);
+            } else {
+                res.status(400).json({ message: 'Could not find project with given id.' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get project' });
+        })
+});
+
+router.post('/', (req, res) => {
+    const projectData = req.body;
+    Projects.insert(projectData)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to create new project' })
+        })
+});
 
 module.exports = router;
